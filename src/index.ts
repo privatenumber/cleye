b779390fdb3bb5a6cf3592afeba38ccab0dfff86
hiroki osame <hiroki.osame@gmail.com>
2022-06-04 02:06:03 -0400

    refactor: combine exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export type { Command } from './command';
-export type { Renderers } from './render-help';
-
 export { cli } from './cli';
-export { command } from './command';
+export { command, type Command } from './command';
+export type { Renderers } from './render-help';
