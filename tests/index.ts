2dd180368aecdda16a94d69c0e37b4c7aa7deeec
Hiroki Osame <hiroki.osame@gmail.com>
2022-10-19 11:37:38 +0900

    test: correct test name

--- tests/index.ts
+++ tests/index.ts
@@ -3,7 +3,7 @@ import { describe } from 'manten';
 // For CLI tools to render without ascii codes
 process.env.CI = '1';
 
-describe('get-tsconfig', ({ runTestSuite }) => {
+describe('cleye', ({ runTestSuite }) => {
 	runTestSuite(import('./specs/cli.js'));
 	runTestSuite(import('./specs/flags.js'));
 	runTestSuite(import('./specs/arguments.js'));
